302b850bdaca6e799312beb83a21bd4a78cec6ff
subortoshuvo <suborto.shuvo@gmail.com>
2020-03-27 20:49:16 +0600

    comment

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop:true,
+         
+         items:1,
+         dots:true,
+         autoplay:false,
+         autoHeight:true,
+         margin:10
+    });
+  });
